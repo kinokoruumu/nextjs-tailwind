5c25d8892af336851d7f4e288d04ee77a46f51bb
kinokoruumu <kinokoruumu@gmail.com>
2023-09-06 23:53:52 +0900

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div className="flex gap-2">
+      <div className="flex gap-2 bg-slate-400 w-[200px]">
         <p>Hello</p>
         <p>World</p>
       </div>
